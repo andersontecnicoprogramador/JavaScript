7984270f27bba597468dc80f8d1b415946caa696
Anderson Moreira <anderson.tecnicoprogramador@hotmail.com>
2023-10-27 05:27:55 -0300

    Aula 1 tipos variaveis,CamelCase...etc

--- aula-06/index.js
+++ aula-06/index.js
@@ -1,2 +1,40 @@
-console.log("Hello World");
-console.log("Estamos na segunda aula");
+//console.log("Hello World");
+//console.log("Estamos na segunda aula");
+
+//Utilize sempre o Camel case no javaScript
+//JavaScript possui tipagem dinâmica:inferi os tipos de dados
+//JavaScript possui uma tipagem fraca 
+
+console.log(curso, topico);
+
+var nomeDoProfessor = 'Anderson Moreira da Silva'//string (texto):Utilize sempre aspas
+var idade = 41 //number
+var altura = 1.77;// number
+var estudando = true;//boolean (booleano: verdeiro ou falso)
+
+console.log(nomeDoProfessor,typeof nomeDoProfessor);
+
+console.log(idade,typeof idade);
+
+console.log(altura,typeof altura);
+
+console.log(estudando,typeof estudando);
+
+var semConteudo;//Declarando uma variavel
+
+console.log(semConteudo);
+
+var curso = "Front-end em React",
+topico = "JavaScript Básico I";
+
+
+//! Não utilize o var nos seus códigos JavaScript
+
+let notaDoAluno = 10;
+const mediaDoAluno = 8;
+
+notaDoAluno = 9;
+//!mediaDoAluno = 5;
+
+console.log(notaDoAluno);
+console.log(mediaDoAluno);
